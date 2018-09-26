2b33879b774528f101a254883a8b18837caa2eba
Shubham <shubham.koli@outlook.in>
2018-09-26 15:46:02 +0530

    Added promise to incrypt the json Object

--- server/EHR/encryption/encryption-promise.js
+++ server/EHR/encryption/encryption-promise.js
@@ -1,4 +1,5 @@
 const Crypto = require("crypto-js");
+
 let encrypt_promise = (str, key) => {
     return new Promise((resolve, reject) => {
         if (str === null || str === undefined) {
@@ -17,6 +18,49 @@ let encrypt_promise = (str, key) => {
 }
 
 
+let encryptObject = (data, FABRIC_KEY) => {
+    return new Promise((resolve, reject) => {
+        for (let key in data) {
+            if (data.hasOwnProperty(key)) {
+
+                if (key == '$class' || key == 'AdharNo') {
+
+                } else {
+                    let val = data[key];
+                    encrypt_promise(val, FABRIC_KEY).then((res) => {
+                        ReplaceWithCipher(data, key, res).then((res) => {
+
+                        }, (errorMessage) => {
+
+                            console.log(errorMessage);
+                            reject(errorMessage);
+                        });
+                    }, (errorMessage) => {
+
+                        console.log(errorMessage);
+                        reject(errorMessage);
+                    });
+                }
+            }
+        }
+        resolve(data);
+    });
+}
+
+let ReplaceWithCipher = (data, key, res) => {
+    return new Promise((resolve, reject) => {
+        if (data == undefined || data == null || key == undefined || key == null || res == undefined || res == null)
+            reject('Invalid Input');
+        else {
+            data[key] = res;
+            resolve(data);
+        }
+    });
+
+}
+
+
 module.exports = {
-    encrypt_promise
+    encrypt_promise,
+    encryptObject
 }
