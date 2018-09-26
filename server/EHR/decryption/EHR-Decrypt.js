2f27fed43f0b74d84d53f7f9e0feeb1fe6387468
Shubham <shubham.koli@outlook.in>
2018-09-26 15:47:05 +0530

    succefully integrated promise in system

--- server/EHR/decryption/EHR-Decrypt.js
+++ server/EHR/decryption/EHR-Decrypt.js
@@ -3,6 +3,19 @@ const decrypt = require("./decryption-promise");
 const Crypto = require("crypto-js");
 const FABRIC_KEY = process.env.FABRIC_KEY;
 
-let cryptoText = 'U2FsdGVkX1+lTU9gLK9C+pOvRfdFkge/05FfhfIkl+Q=';
-let text = decrypt.decrypt_promise(cryptoText, FABRIC_KEY);
-console.log(text);
+let data = {
+    '$class': 'org.example.basic.Patient',
+    'AdharNo': '1234',
+    'firstName': 'U2FsdGVkX1+rQcgvWmV5wilqrx3ZfQtuk8SyvmJQdcc=',
+    'lastName': 'U2FsdGVkX19iQWorz9T+ML0l4ylyTL3ZEXZrOWMBO3Q=',
+    'Dob': 'U2FsdGVkX19tmA4kNYEoDY3xUeXM2wjKoQWYl7ek3Vc=',
+    'BloodGroup': 'U2FsdGVkX1+g2ZarNQS4gUFNbl0X8AbLmqDY6kUCtoY=',
+    'Gender': 'U2FsdGVkX1/GDjvdkmryI+A1uiSL0BWZhqX0m/Pyf9Q=',
+    'address': 'U2FsdGVkX1+jxJGwo0IRYinalOOs88AqbBiCkkNcgDI=',
+    'pincode': 'U2FsdGVkX1+b8NS6b8F1iED5PtBLrUHZlHS1koNDlOs='
+}
+decrypt.decryptObject(data, FABRIC_KEY).then((res) => {
+    console.log(res);
+}, (err) => {
+    console.log(err);
+});
