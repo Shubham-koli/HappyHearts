9fb753a77f83496fd4f25cb54a161856615c9c8c
Shubham <shubham.koli@outlook.in>
2018-09-26 15:46:47 +0530

    succefully integrated promise in the system

--- server/EHR/encryption/EHR-Encrypt.js
+++ server/EHR/encryption/EHR-Encrypt.js
@@ -3,6 +3,19 @@ const encrypt = require("./encryption-promise");
 const Crypto = require("crypto-js");
 const FABRIC_KEY = process.env.FABRIC_KEY;
 
-let plainText = 'shubham';
-let text = encrypt.encrypt_promise(plainText, FABRIC_KEY);
-console.log(text);
+let data = {
+    "$class": "org.example.basic.Patient",
+    "AdharNo": "1234",
+    "firstName": "shubham",
+    "lastName": "koli",
+    "Dob": "31/10/1995",
+    "BloodGroup": "B+",
+    "Gender": "M",
+    "address": "Solapur",
+    "pincode": ""
+};
+encrypt.encryptObject(data, FABRIC_KEY).then((res) => {
+    console.log(res);
+}, (err) => {
+    console.log(err);
+});
