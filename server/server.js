6f58514c4ce9a369180a919494ba4051741bae3c
Shubham <shubham.koli@outlook.in>
2018-09-27 12:29:17 +0530

    Added API end-points encrypt and decrypt.(temparory)

--- server/server.js
+++ server/server.js
@@ -2,11 +2,44 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
-const crypto = require("crypto-js");
+
+const {
+    encryptUsingFABRIC_KEY
+} = require("./EHR/encryption/Fabric-Encrypt");
+const {
+    encryptUsingPrivateKey
+} = require("./EHR/encryption/EHR-Encrypt");
+const {
+    decryptUsingFABRIC_KEY
+} = require("./EHR/decryption/Fabric-Decrypt");
+const {
+    decryptUsingPrivateKey
+} = require("./EHR/decryption/EHR-Decrypt");
 
 var app = express();
 app.use(bodyParser.json());
 
+app.post('/encrypt', (req, res) => {
+    console.log('encrypting using private key');
+    encryptUsingPrivateKey(req.body).then((result) => {
+        console.log('encrypting using Fabric key');
+        encryptUsingFABRIC_KEY(result).then((encryptedObj) => {
+            console.log('Object Successfully Encrypted');
+            res.send(encryptedObj);
+        });
+    });
+});
+
+app.get("/decrypt", (req, res) => {
+    console.log("decrypt using Fabric Key");
+    decryptUsingFABRIC_KEY(req.body).then((result) => {
+        console.log("decrypt using Private Key");
+        decryptUsingPrivateKey(result).then((decryptedObj) => {
+            console.log("Object Successfully Decrypted");
+            res.send(decryptedObj);
+        });
+    })
+});
 
 app.listen(3000, () => {
     console.log("Started on port 3000");
