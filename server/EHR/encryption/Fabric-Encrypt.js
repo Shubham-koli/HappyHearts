e2cda008439599d2b6e9fb48865ad6f077968edb
Shubham <shubham.koli@outlook.in>
2018-09-27 12:28:29 +0530

    created a module which encrypts data using Fabric_Key

--- server/EHR/encryption/Fabric-Encrypt.js
+++ server/EHR/encryption/Fabric-Encrypt.js
@@ -7,17 +7,17 @@ const {
 const FABRIC_KEY = process.env.FABRIC_KEY;
 
 
-let data = {
-    "$class": "org.example.basic.Patient",
-    "AdharNo": "8421999884",
-    "firstName": "shubham",
-    "lastName": "koli",
-    "Dob": "31/10/1995",
-    "BloodGroup": "B+",
-    "Gender": "M",
-    "address": "Solapur",
-    "pincode": ""
-};
+// let data = {
+//     "$class": "org.example.basic.Patient",
+//     "AdharNo": "8421999884",
+//     "firstName": "shubham",
+//     "lastName": "koli",
+//     "Dob": "31/10/1995",
+//     "BloodGroup": "B+",
+//     "Gender": "M",
+//     "address": "Solapur",
+//     "pincode": ""
+// };
 
 let encryptUsingFABRIC_KEY = (data) => {
     return new Promise((resolve, reject) => {
