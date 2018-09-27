db7db39413c477e32dfd550669b172f87ec6640d
Shubham <shubham.koli@outlook.in>
2018-09-27 12:26:27 +0530

    created a new Promise which decrypts data using users private key

--- server/EHR/decryption/EHR-Decrypt.js
+++ server/EHR/decryption/EHR-Decrypt.js
@@ -1,21 +1,39 @@
 require("dotenv").config();
-const decrypt = require("./decryption-promise");
-const Crypto = require("crypto-js");
-const FABRIC_KEY = process.env.FABRIC_KEY;
+const {
+    decryptObject
+} = require("./decryption-promise");
+const {
+    getKey
+} = require("../MongoDB/GetKeys");
 
-let data = {
-    '$class': 'org.example.basic.Patient',
-    AdharNo: '1234',
-    firstName: 'U2FsdGVkX19gHkmbVGpTbVxvFOUbhHFpG5NUUoxnyVU=',
-    lastName: 'U2FsdGVkX19R1hHAqS2x9hmQMbdMKTk0GGOtPMiBLJo=',
-    Dob: 'U2FsdGVkX1+P95reW74sOdZ2QcCOvEGSkrLe6+Rh+7k=',
-    BloodGroup: 'U2FsdGVkX1+4+k4yuroSyv1/UBdvfHvTj3QnDDnywMY=',
-    Gender: 'U2FsdGVkX1+tXK7usTDMc7ZEgnul7WpBgG5fdquEWQQ=',
-    address: 'U2FsdGVkX19Wa/kmYikTpK4I+ttfDvR6OdF0IvcCN78=',
-    pincode: 'U2FsdGVkX1+6cNNDbZh3jfzxsKjRe6R73qgQeOlGwTc='
-}
-decrypt.decryptObject(data, FABRIC_KEY).then((res) => {
-    console.log(res);
-}, (err) => {
-    console.log(err);
-});
+
+
+// let data = {
+//     '$class': 'org.example.basic.Patient',
+//     AdharNo: '8421999884',
+//     firstName: 'U2FsdGVkX19rWlqTuFQTv6sJTgh3LBr5rpdMWhOEw2k=',
+//     lastName: 'U2FsdGVkX19WUZolZCFCLz9DAMV/NhGuDseSyT8glgY=',
+//     Dob: 'U2FsdGVkX19vaXBrZFT4KdD0G4VeOk7JjYv25rpOWgU=',
+//     BloodGroup: 'U2FsdGVkX1+dTu8xMj+68478F2chibGa1t+FvmqsvTE=',
+//     Gender: 'U2FsdGVkX18ydT3jYNIDEiGQGG7beWc1yZVsYmXchy4=',
+//     address: 'U2FsdGVkX1/H4Cy/zrDSKwD79czOJmlt+ACD8m1FQ14=',
+//     pincode: 'U2FsdGVkX19nFiSgLlqmcvabWQ2dxJiYMjL872JJX0I='
+// };
+let decryptUsingPrivateKey = (data) => {
+    return new Promise((resolve, reject) => {
+        getKey(data.AdharNo).then((res) => {
+            decryptObject(data, res).then((result) => {
+                // console.log(result);
+                resolve(result);
+            });
+        })
+    })
+};
+
+// decryptUsingPrivateKey(data).then((res) => {
+//     console.log(res);
+// })
+
+module.exports = {
+    decryptUsingPrivateKey // It decrypts Data using users private key stored in MongoDB
+};
