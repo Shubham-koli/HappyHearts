70bd75315c6a19da08b35be03a1c39f6bbd6af9a
Shubham <shubham.koli@outlook.in>
2018-09-27 12:25:18 +0530

    created new promise which encrypts data by Fabric_Key

---
+++ server/EHR/encryption/Fabric-Encrypt.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+
+const {
+    encryptObject
+} = require("./encryption-promise");
+
+const FABRIC_KEY = process.env.FABRIC_KEY;
+
+
+let data = {
+    "$class": "org.example.basic.Patient",
+    "AdharNo": "8421999884",
+    "firstName": "shubham",
+    "lastName": "koli",
+    "Dob": "31/10/1995",
+    "BloodGroup": "B+",
+    "Gender": "M",
+    "address": "Solapur",
+    "pincode": ""
+};
+
+let encryptUsingFABRIC_KEY = (data) => {
+    return new Promise((resolve, reject) => {
+        encryptObject(data, FABRIC_KEY).then((res) => {
+            // console.log(res);
+            resolve(res);
+        })
+    })
+};
+// encryptUsingFABRIC_KEY(data, FABRIC_KEY).then((res) => {
+//     console.log(res);
+// })
+
+module.exports = {
+    encryptUsingFABRIC_KEY // It encrypts (By Fabric Key) already encrypted (By User's Private key) data 
+}
