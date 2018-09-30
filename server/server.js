d281ea00b2179fa1df517022267c075e000f5184
Shubham <shubham.koli@outlook.in>
2018-09-30 10:36:16 +0530

    Added checkAccess functionality adn two new routes

--- server/server.js
+++ server/server.js
@@ -38,6 +38,12 @@ const {
   createAccessRecord //// it creates new Asset of Record (Patient's EHR Access Record)
 } = require("./routes/createAssets");
 
+const {
+  accessGRANT,
+  accessReq,
+  accessDENY,
+  checkAccess
+} = require("./EHR/MongoDB/Consent");
 var app = express();
 app.use(bodyParser.json());
 
@@ -67,19 +73,36 @@ app.get("/decrypt", (req, res) => {
   });
 });
 
-app.get("/getpatient/:id", (req, res) => {
-  let AadharNo = req.params.id;
-  getPatient(AadharNo)
-    .then(result => {
-      console.log("decrypt using Fabric Key");
-      decryptUsingFABRIC_KEY(result).then(result1 => {
-        console.log("decrypt using Private Key");
-        decryptUsingPrivateKey(result1).then(decryptedObj => {
-          console.log("Object Successfully Decrypted");
-          res.send(decryptedObj);
-        });
-      });
-    })
+app.post("/getpatient", (req, res) => {
+  let AadharNo = req.body.AdharNo;
+  let Hospital_ID = req.body.Hospital_ID;
+  console.log(AadharNo);
+  checkAccess(AadharNo, Hospital_ID)
+    .then(
+      code => {
+        if (code == "200") {
+          getPatient(AadharNo)
+            .then(result => {
+              console.log("decrypt using Fabric Key");
+              decryptUsingFABRIC_KEY(result).then(result1 => {
+                console.log("decrypt using Private Key");
+                decryptUsingPrivateKey(result1).then(decryptedObj => {
+                  console.log("Object Successfully Decrypted");
+                  res.send(decryptedObj);
+                });
+              });
+            })
+            .catch(err => {
+              console.log(err);
+            });
+        } else {
+          res.sendStatus(401);
+        }
+      },
+      code => {
+        res.sendStatus(401);
+      }
+    )
     .catch(err => {
       console.log(err);
     });
@@ -133,73 +156,163 @@ app.post("/newpatient", (req, res) => {
     });
 });
 
-app.post("/treatment", (req, res) => {
-  mongoEncrypt(req.body)
+app.post("/newtreatment", (req, response) => {
+  let AadharNo = req.body.patientData.substr(39);
+  let Hospital_ID = req.body.HospitalName;
+  checkAccess(AadharNo, Hospital_ID)
+    .then(code => {
+      if (code == "200") {
+        mongoEncrypt(req.body)
+          .then(
+            res => {
+              console.log("Treatment saved in the MongoDB");
+            },
+            errorMessage => {
+              console.log(
+                "error while saving Treatment Details to MongoDB\n",
+                errorMessage
+              );
+            }
+          )
+          .catch(err => {
+            console.log(err);
+          });
+        console.log("entering treatment details");
+        console.log(req.body);
+        encrypt_TreatmentDetails_UsingPrivateKey(req.body).then(result1 => {
+          console.log("encrypting using Private key");
+          encrypt_TreatmentDetails_UsingFABRIC_KEY(result1).then(result => {
+            console.log("encrypting using Fabric key");
+            addTreatmentDetails(result)
+              .then(
+                res => {
+                  console.log(res);
+                  response.sendStatus(200);
+                },
+                errorMessage => {
+                  console.log(errorMessage);
+                  response.sendStatus(500);
+                }
+              )
+              .catch(errorMessage => {
+                console.log(errorMessage);
+              });
+          });
+        });
+      } else {
+        response.sendStatus(401);
+      }
+    })
+    .catch(err => {
+      response.sendStatus(401);
+    });
+});
+
+app.post("/treatment", (req, response) => {
+  let AadharNo = req.body.AdharNo;
+  let Hospital_ID = req.body.Hospital_ID;
+  checkAccess(AadharNo, Hospital_ID)
+    .then(code => {
+      if (code == "200") {
+        getPatientData(AadharNo)
+          .then(
+            result => {
+              console.log("Fetching Data from Blockchain");
+              decrypt_TreatmentDetails_UsingFabricKey(result).then(result1 => {
+                console.log("decrypted using Fabric Key");
+                decrypt_TreatmentDetails_UsingPrivateKey(result1).then(
+                  decryptedObj => {
+                    console.log("decrypt using Private Key");
+                    response.send(decryptedObj);
+                  }
+                );
+              });
+            },
+            errorMessage => {
+              console.log(errorMessage);
+              response.sendStatus(404);
+            }
+          )
+          .catch(errorMessage => {
+            console.log(errorMessage);
+            response.sendStatus(404);
+          });
+      } else {
+        response.sendStatus(401);
+      }
+    })
+    .catch(err => {
+      response.sendStatus(401);
+    });
+});
+
+app.post("/grant/", (req, response) => {
+  console.log(req.body.AdharNo);
+  accessGRANT(req.body)
     .then(
       res => {
-        console.log("Treatment saved in the MongoDB");
+        getPatient(req.body.AdharNo)
+          .then(result => {
+            console.log("decrypt using Fabric Key");
+            decryptUsingFABRIC_KEY(result).then(result1 => {
+              console.log("decrypt using Private Key");
+              decryptUsingPrivateKey(result1).then(decryptedObj => {
+                console.log("Object Successfully Decrypted");
+                response.send(decryptedObj);
+              });
+            });
+          })
+          .catch(err => {
+            console.log(err);
+          });
       },
       errorMessage => {
         console.log(
-          "error while saving Treatment Details to MongoDB\n",
+          "error while inserting access grant in MongoDB",
           errorMessage
         );
       }
     )
     .catch(err => {
-      console.log(err);
+      console.log("error while inserting access grant in MongoDB", err);
     });
-  console.log("entering treatment details");
-  console.log(req.body);
-  encrypt_TreatmentDetails_UsingPrivateKey(req.body).then(result1 => {
-    console.log("encrypting using Private key");
-    encrypt_TreatmentDetails_UsingFABRIC_KEY(result1).then(result => {
-      console.log("encrypting using Fabric key");
-      addTreatmentDetails(result)
-        .then(
-          response => {
-            console.log(response);
-            res.sendStatus(200);
-          },
-          errorMessage => {
-            console.log(errorMessage);
-            res.sendStatus(500);
-          }
-        )
-        .catch(errorMessage => {
-          console.log(errorMessage);
-        });
-    });
-  });
 });
 
-app.get("/treatment/:id", (req, res) => {
-  getPatientData(req.params.id)
+app.post("/request", (req, res) => {
+  accessReq(req.body)
     .then(
-      result => {
-        console.log("Fetching Data from Blockchain");
-        decrypt_TreatmentDetails_UsingFabricKey(result).then(result1 => {
-          console.log("decrypted using Fabric Key");
-          decrypt_TreatmentDetails_UsingPrivateKey(result1).then(
-            decryptedObj => {
-              console.log("decrypt using Private Key");
-              res.send(decryptedObj);
-            }
-          );
-        });
+      res => {
+        console.log("Access Request Created");
+        res.sendStatus(200);
       },
       errorMessage => {
         console.log(errorMessage);
-        res.sendStatus(404);
+        res.sendStatus(409);
       }
     )
     .catch(errorMessage => {
       console.log(errorMessage);
-      res.sendStatus(404);
+      res.sendStatus(409);
     });
 });
 
-app.post("/analytics/:domain/:id", (req, res) => {});
+app.post("/deny", (req, response, next) => {
+  accessDENY(req.body)
+    .then(
+      res => {
+        console.log("Access Request DENIED");
+        response.sendStatus(200);
+      },
+      errorMessage => {
+        console.log(errorMessage);
+        response.sendStatus(409);
+      }
+    )
+    .catch(errorMessage => {
+      console.log(errorMessage);
+      response.sendStatus(404);
+    });
+});
 
 app.listen(4000, () => {
   console.log("Started on port 4000");
