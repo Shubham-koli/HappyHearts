cc052904e25a6be24b4af48996d7b936b5274b69
Shubham <shubham.koli@outlook.in>
2018-09-30 14:38:22 +0530

    Added modules that logs access records into the blockchain

---
+++ server/routes/storeRecord.js
@@ -0,0 +1,62 @@
+const REST_URL = "http://localhost:3000/api";
+const axios = require("axios");
+
+// sample input data
+// {
+//   "AdharNo": "1337",
+//   "Hospital_ID":"HSTL123",
+//   "Staff_ID":"Dr Koli"
+//  }
+
+let denyRecord = data => {
+  return new Promise((resolve, reject) => {
+    axios
+      .post(`${REST_URL}/org.example.basic.Record`, {
+        $class: "org.example.basic.Record",
+        HospitalName: data.Hospital_ID,
+        StaffId: data.Staff_ID,
+        AccessType: "DENY",
+        accessRecord: "resource:org.example.basic.AccessRecord#" + data.AdharNo
+      })
+      .then(
+        function(response) {
+          resolve(200);
+        },
+        error => {
+          reject(404);
+        }
+      )
+      .catch(function(error) {
+        reject(500);
+      });
+  });
+};
+
+let grantRecord = data => {
+  return new Promise((resolve, reject) => {
+    axios
+      .post(`${REST_URL}/org.example.basic.Record`, {
+        $class: "org.example.basic.Record",
+        HospitalName: data.Hospital_ID,
+        StaffId: data.Staff_ID,
+        AccessType: "GRANT",
+        accessRecord: "resource:org.example.basic.AccessRecord#" + data.AdharNo
+      })
+      .then(
+        function(response) {
+          resolve(200);
+        },
+        error => {
+          reject(404);
+        }
+      )
+      .catch(function(error) {
+        reject(500);
+      });
+  });
+};
+
+module.exports = {
+  grantRecord,
+  denyRecord
+};
