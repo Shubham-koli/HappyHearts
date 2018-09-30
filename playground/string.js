0a368d5e6da9ca6ed7fb1614441468de9752c20d
Shubham <shubham.koli@outlook.in>
2018-09-30 10:35:52 +0530

    testing

--- playground/string.js
+++ playground/string.js
@@ -1,10 +1,10 @@
 let treatmentData = {
-    $class: "org.example.basic.TreatmentDetails",
-    HospitalName: "Civil Solapur",
-    StaffId: "1234",
-    PinCode: "413001",
-    patientData: "resource:org.example.basic.PatientData#3893"
+  $class: "org.example.basic.TreatmentDetails",
+  HospitalName: "Civil Solapur",
+  StaffId: "1234",
+  PinCode: "413001",
+  accessRecord: "resource:org.example.basic.PatientData#1337"
 };
 
-let str = treatmentData.patientData.substr(39);
+let str = treatmentData.accessRecord.substr(39);
 console.log(str);
