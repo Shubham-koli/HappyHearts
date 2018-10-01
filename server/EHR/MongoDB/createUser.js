fbb9bdf9415aeb7374dd3e44829689943b644d1a
Shubham <shubham.koli@outlook.in>
2018-10-02 01:19:30 +0530

    creating new user with lastname

---
+++ server/EHR/MongoDB/createUser.js
@@ -0,0 +1,25 @@
+const { mongoose } = require("./mongoose");
+const { Patient } = require("../models/Patient");
+
+let addNewPatient = (AdharNo, firstName, lastName) => {
+  return new Promise((resolve, reject) => {
+    let user = new Patient({
+      _id: AdharNo,
+      name: firstName + " " + lastName
+    });
+    user.save().then(
+      doc => {
+        console.log(doc);
+        resolve(200);
+      },
+      err => {
+        console.log(err);
+        resolve(500);
+      }
+    );
+  });
+};
+
+module.exports = {
+  addNewPatient
+};
