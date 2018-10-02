ffdbddae8db020677013597b666dc29ace635915
Shubham <shubham.koli@outlook.in>
2018-10-02 23:04:40 +0530

    well fuck

--- playground/mongoDecrypt.js
+++ playground/mongoDecrypt.js
@@ -1,19 +1,6 @@
 const { Treatment } = require("../server/EHR/models/AnalyticalData");
 const { mongoose } = require("../server/EHR/MongoDB/mongoose");
 console.log("test");
-Treatment.find().then(data => {
-  // console.log(data.length);
-  data.forEach(function(obj) {
-    for (let record in obj) {
-      if (obj.hasOwnProperty(record)) {
-        console.log({ record });
-      }
-    }
-  });
-}),
-  errorMessage => {
-    console.log(errorMessage);
-  };
 
 // for (let record in data) {
 //     if (data.hasOwnProperty(record)) {
