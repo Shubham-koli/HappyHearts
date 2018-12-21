e795c7561e788e5dd901c4522897e22681aff13a
Shubham Koli <shubham.koli@outlook.in>
2018-12-21 18:14:49 +0530

    commented the code

--- server/Insurer/getClients.js
+++ server/Insurer/getClients.js
@@ -27,11 +27,11 @@ let getClients = (insurer) => {
     })
 }
 
-getClients('Bajaj').then(result => {
-    console.log(result);
-}).catch(err => {
-    console.log(err);
-})
+// getClients('Bajaj').then(result => {
+//     console.log(result);
+// }).catch(err => {
+//     console.log(err);
+// })
 
 
 module.exports = {
