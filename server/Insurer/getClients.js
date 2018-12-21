f8605cdc0f279e85338dd1c3b96fcbb98ffe087a
Shubham Koli <shubham.koli@outlook.in>
2018-12-21 17:24:12 +0530

    added module to get cleints for insurer

---
+++ server/Insurer/getClients.js
@@ -0,0 +1,39 @@
+const {
+    mongoose
+} = require("../EHR/MongoDB/mongoose");
+
+const {
+    Claim
+} = require("../EHR/models/claimStatus");
+
+const _ = require("lodash");
+
+let getClients = (insurer) => {
+    return new Promise((resolve, reject) => {
+        let result = [];
+        Claim.find().then(doc => {
+            doc.forEach(record => {
+                let res = _.find(record.Treatment, {
+                    claimStatus: "UNDER PROCESS",
+                    insurer: insurer
+                })
+                result.push(res);
+            })
+            result = _.compact(result)
+            resolve(result)
+        }).catch(err => {
+            reject(err);
+        })
+    })
+}
+
+// getClients('Bajaj').then(result => {
+//     console.log(result);
+// }).catch(err => {
+//     console.log(err);
+// })
+
+
+module.exports = {
+    getClients
+}
